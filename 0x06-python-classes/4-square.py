f94168547b895ef1b36276d26c861788390d7ad2
yunstarry <yungstarry@yahoo.com>
2022-12-21 01:06:01 -0800

    class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self._size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self._size = value
+
+    def area(self):
+        return self._size ** 2
+
